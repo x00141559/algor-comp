fd465d095ed0722932df7630c75350ce4d3ae9fd
X00141559 Aoife Bergin <x00141559@COMPUTING.STU.IT-TALLAGHT.IE>
2020-02-17 17:16:12 +0000

    lab 4

---
+++ lab4/BinarySTree.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "TreeNode.h"
+class BinarySTree
+{
+public:
+	BinarySTree();
+	~BinarySTree();
+	void print();
+	void add(int toAdd);
+	int height() const;
+	int minValue();
+	int maxValue();
+	int isBST();
+	int isBST2();
+private:
+	TreeNode* root;
+	void addNode(TreeNode* at, TreeNode* toAdd);
+	void print(TreeNode* p);
+	int height(TreeNode* h) const;
+	int minValue(TreeNode* node);	int maxValue(TreeNode* node);
+	int isBST(TreeNode* node);
+	int isBST2(TreeNode* node);	int isBSTUtil(TreeNode* node, int min, int max);
+};
